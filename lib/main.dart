50e1316b181cb39849462fe7b2b7e2011f284fe6
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-02-28 16:18:12 +0900

    add: html/parserを入れてみて、動作確認をしてみました

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:html/parser.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,12 +31,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    //TODO イメージとしては、HTMLタグとHeadタグを自動でつけてくれるものっぽい・・？
+    var document =
+        parse('<body>Hello world! <a href="www.html5rocks.com">HTML5 rocks!');
+    print(document.outerHtml);
+    //<html><head></head><body>Hello world! <a href="www.html5rocks.com">HTML5 rocks!</a></body></html>
   }
 
   @override
@@ -45,16 +46,12 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'You have pushed the button this many times:',
             ),
           ],
         ),
